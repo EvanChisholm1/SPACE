d94585e788a6a8aeb6f7aa9b89c22a49b94002da
Evan Chisholm <elchisholm248@gmail.com>
2024-05-13 20:49:14 -0400

    added n body problem simulation

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,13 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 // const PIXELS_PER_METER = canvas.width / WIDTH_IN_METERS;
 // console.log(PIXELS_PER_METER, 50e2);
 
-const WIDTH_IN_METERS = 100;
+const WIDTH_IN_METERS = 5e8;
 const PIXELS_PER_METER = canvas.width / WIDTH_IN_METERS;
 console.log(PIXELS_PER_METER, 50e2);
+
+// const WIDTH_IN_METERS = 100;
+// const PIXELS_PER_METER = canvas.width / WIDTH_IN_METERS;
+// console.log(PIXELS_PER_METER, 50e2);
 
 // console.log(canvas.width / PIXELS_PER_METER);
 
@@ -52,7 +56,96 @@ function threeBodyProblem() {
     });
 }
 
+function nBodyProblem() {
+    const M = 6e31;
+    const bodies = [
+        new Body({
+            position: { x: 50e6, y: 50e6 },
+            velocity: { x: 10, y: -7e6 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "green",
+        }),
+        new Body({
+            position: { x: 50e6, y: 100e6 },
+            velocity: { x: -10e6, y: -7 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "red",
+        }),
+        new Body({
+            position: { x: 100e6, y: 50e6 },
+            velocity: { x: 10, y: 10e6 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "blue",
+        }),
+        new Body({
+            position: { x: 100e6, y: 100e6 },
+            velocity: { x: -10e5, y: 10e6 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "pink",
+        }),
+        new Body({
+            position: { x: 20e6, y: 100e6 },
+            velocity: { x: -10e5, y: 10e6 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "orange",
+        }),
+        new Body({
+            position: { x: 20e6, y: 10e6 },
+            velocity: { x: -10e5, y: 10e6 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "yellow",
+        }),
+    ];
+
+    for (let i = 0; i < 2; i++) {
+        bodies.push(
+            new Body({
+                position: { x: Math.random() * 5e8, y: Math.random() * 5e8 },
+                velocity: { x: Math.random() * 10e5, y: Math.random() * 10e5 },
+                mass: M,
+                radius: 3e6,
+                generateGravity: true,
+                color: "white",
+            })
+        );
+    }
+
+    return new Simulator(bodies, {
+        x: { min: 0, max: canvas.width / PIXELS_PER_METER },
+        y: { min: 0, max: canvas.height / PIXELS_PER_METER },
+    });
+}
+
 // const simulator = threeBodyProblem();
+const simulator = nBodyProblem();
+
+function spawn() {
+    const M = 6e31;
+
+    simulator.bodies.push(
+        new Body({
+            position: { x: Math.random() * 5e8, y: Math.random() * 5e8 },
+            velocity: { x: Math.random() * 10e5, y: Math.random() * 10e5 },
+            mass: M,
+            radius: 3e6,
+            generateGravity: true,
+            color: "purple",
+        })
+    );
+    console.log(simulator.bodies.length);
+}
 
 function orbit() {
     const bodies = [
@@ -236,7 +329,20 @@ function walls() {
     );
 }
 
-const simulator = walls();
+// const simulator = walls();
+
+const spawnButton = document.querySelector("#spawn");
+
+spawnButton?.addEventListener("click", spawn);
+
+function kineticEnergy(body: Body) {
+    const speed = Math.sqrt(body.velocity.x ** 2 + body.velocity.y ** 2);
+    return (1 / 2) * (body.mass * speed ** 2);
+}
+
+function findTotalKinetic() {
+    return simulator.bodies.reduce((acc, body) => acc + kineticEnergy(body), 0);
+}
 
 let prevTime = performance.now();
 let frameCount = 0;
@@ -252,6 +358,7 @@ function loop() {
     simulator.step(dt);
     simulator.render(ctx, PIXELS_PER_METER);
 
+    console.log(findTotalKinetic());
     requestAnimationFrame(loop);
 }
 
